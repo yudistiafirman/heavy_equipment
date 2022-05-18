7272fdd35fbbbb50f3c82e447a3890ea05fc4c26
Yudistia Firman <firman.yudistia@codemi.co.id>
2022-05-18 19:42:00 +0700

    add clients

--- src/Components/Home/Client.jsx
+++ src/Components/Home/Client.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import prima from './assets/prima.jpg'
 import putra from './assets/putra.jpg'
+import ricobana from './assets/ricobana.png'
+import sany from './assets/sany.png'
+import safety from './assets/safety.png'
 const Client = () => {
-    const logo = [prima,putra]
+    const logo = [prima,putra,ricobana,sany,safety]
     return (
         <div className='clientContainer'>
             {
@@ -10,7 +13,7 @@ const Client = () => {
                     return    <div key={i}    style={{
                         backgroundImage:`url(${v})`,
                         backgroundRepeat:'no-repeat',
-                        backgroundSize: "cover",
+                        backgroundSize: "contain",
                         backgroundPosition: "center",
                       }}  className="logo">
         
